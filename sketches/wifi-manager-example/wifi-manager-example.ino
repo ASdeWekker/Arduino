02c1192679487bfb5c98f9349bf17bcf485deb07
Alex de Wekker <alexdewekker6@gmail.com>
2019-03-24 18:56:00 +0100

    typo and other fixes

--- sketches/wifi-manager-example/wifi-manager-example.ino
+++ sketches/wifi-manager-example/wifi-manager-example.ino
@@ -9,7 +9,7 @@ ESP8266WebServer server(80);
 
 // Index page.
 void handleRoot() {
-  String html ="<html> <head> <meta name=\"viewport\" content=\"width=device width, initial-scale=1\"> <title>Ding</title> </head> <body> <h1>Dingetje</h1> <p><a href=\"http://192.168.1.71/led?led=aan\">Aan</a></p> <p><a href=\"http://192.168.1.71/led?led=uit\">Uit</a></p> <p><a href=\"http://192.168.1.71/led?led=toggle\">Toggle</a></p> </body> </html>";
+  String html ="<html> <head> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> <title>Ding</title> </head> <body> <h1>Dingetje</h1> <p><button onclick=\"aan()\">Aan</button></p> <p><button onclick=\"uit()\">Uit</button></p> <p><button onclick=\"toggle()\">Toggle</button></p> <script type=\"text/javascript\"> var xhttp = new XMLHttpRequest() function aan() { xhttp.open(\"GET\", \"http://192.168.1.71/led?led=aan\", true) //history.pushState(\"\", \"\", \"/netjesa\") xhttp.send() } function uit() { xhttp.open(\"GET\", \"http://192.168.1.71/led?led=uit\", true) //history.pushState(\"\", \"\", \"/netjesu\") xhttp.send() } function toggle() { xhttp.open(\"GET\", \"http://192.168.1.71/led?led=toggle\", true) //history.pushState(\"\", \"\", \"/netjest\") xhttp.send() } </script> </body> </html>";
   server.send(200, "text/html", html);
 }
 
