ea658ca7ea7265de8671fbf6f8bccb9bb462b3c9
Alex de Wekker <alexdewekker6@gmail.com>
2019-03-24 13:35:06 +0100

    working example with 3 potentiometers

--- fastled-example/fastled-example.ino
+++ fastled-example/fastled-example.ino
@@ -1,20 +1,38 @@
-/*#include <FastLED.h>
+// Het is dus als volgt:
+// Rood, Blauw, Groen.
 
-#define NUM_LEDS 300
+#include <FastLED.h>
+
+#define NUM_LEDS 5
 #define DATA_PIN D4
 
-CRGB led[NUM_LEDS];
+int potrpin = D2;
+int potgpin = D1;
+int potbpin = A0;
+
+CRGB leds[NUM_LEDS];
 
 void setup() {
+  Serial.begin(230400);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(30);
 }
-*/
 
-#include "FastLED.h"
-#define NUM_LEDS 60
-CRGB leds[NUM_LEDS];
-void setup() { FastLED.addLeds<NEOPIXEL, D4>(leds, NUM_LEDS); }
 void loop() {
-  leds[0] = CRGB::White; FastLED.show(); delay(30);
-  leds[0] = CRGB::Black; FastLED.show(); delay(30);
+  int potr = analogRead(potrpin);
+  int potg = analogRead(potgpin);
+  int potb = analogRead(potbpin);
+
+  int potrval = map(potr, 0, 1023, 0, 255);
+  int potgval = map(potg, 0, 1023, 0, 255);
+  int potbval = map(potb, 0, 1023, 0, 255);
+  
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(potrval, potbval, potgval);
+    FastLED.show();
+    Serial.print("Potr: "); Serial.print(potr); Serial.print(", Potrval: "); Serial.println(potrval);
+    Serial.print("Potg: "); Serial.print(potg); Serial.print(", Potgval: "); Serial.println(potgval);
+    Serial.print("Potb: "); Serial.print(potb); Serial.print(", Potbval: "); Serial.println(potbval);
+    delayMicroseconds(1);
+  }
 }
