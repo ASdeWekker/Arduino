fbadda15d0f826ee3ee04679362c226f0f4d90b5
Alex de Wekker <alexdewekker6@gmail.com>
2019-03-24 18:34:10 +0100

    working webpage, but not yet fully working

--- sketches/wifi-manager-example/wifi-manager-example.ino
+++ sketches/wifi-manager-example/wifi-manager-example.ino
@@ -3,29 +3,49 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 
-#define led D4
+#define ledpin D4
 
 ESP8266WebServer server(80);
 
+// Index page.
 void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "hello from mij");
-  digitalWrite(led, 0);
+  String html ="<html> <head> <meta name=\"viewport\" content=\"width=device width, initial-scale=1\"> <title>Ding</title> </head> <body> <h1>Dingetje</h1> <p><a href=\"http://192.168.1.71/led?led=aan\">Aan</a></p> <p><a href=\"http://192.168.1.71/led?led=uit\">Uit</a></p> <p><a href=\"http://192.168.1.71/led?led=toggle\">Toggle</a></p> </body> </html>";
+  server.send(200, "text/html", html);
+}
+
+// Led request handling.
+void ledje() {
+  if (server.arg("led") == "aan") {
+    digitalWrite(ledpin, HIGH);
+  } else if (server.arg("led") == "uit") {
+    digitalWrite(ledpin, LOW);
+  } else if (server.arg("led") == "toggle") {
+    if (digitalRead(ledpin) == HIGH) {
+      digitalWrite(ledpin, LOW);
+    } else {
+      digitalWrite(ledpin, HIGH);
+    }
+  }
 }
 
 void setup() {
+  // Setup basic stuff.
   Serial.begin(230400);
-  pinMode(led, OUTPUT);
+  pinMode(ledpin, OUTPUT);
 
+  // Wifi manager setup.
   WiFiManager wifiManager;
-  wifiManager.autoConnect("(_)_)::::::::D~~~", "wemos");
+  wifiManager.autoConnect("(_)_)::::::::D~~~", "password");
 
+  // Wifi/server stuff.
   Serial.println(WiFi.localIP());
+  server.on("/led", ledje);
   server.on("/", handleRoot);
   server.begin();
   Serial.println("Server started");
 }
 
 void loop() {
+  // Call the server
   server.handleClient();
 }
