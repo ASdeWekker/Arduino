ba2bd9203ca39b95cf35b2d65b3d80b1fc9edd4d
Alex de Wekker <alexdewekker6@gmail.com>
2019-03-24 22:40:25 +0100

    changed titles

--- sketches/wifi-manager-example/wifi-manager-example.ino
+++ sketches/wifi-manager-example/wifi-manager-example.ino
@@ -9,7 +9,7 @@ ESP8266WebServer server(80);
 
 // Index page.
 void handleRoot() {
-  String html ="<html> <head> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> <title>Ding</title> </head> <body> <h1>Dingetje</h1> <p><button onclick=\"aan()\">Aan</button></p> <p><button onclick=\"uit()\">Uit</button></p> <p><button onclick=\"toggle()\">Toggle</button></p> <script type=\"text/javascript\"> var xhttp = new XMLHttpRequest(); function aan() { xhttp.open(\"GET\", \"http://192.168.1.71/led?led=aan\", true); xhttp.send(); } function uit() { xhttp.open(\"GET\", \"http://192.168.1.71/led?led=uit\", true); xhttp.send(); } function toggle() { xhttp.open(\"GET\", \"http://192.168.1.71/led?led=toggle\", true); xhttp.send(); } </script> </body> </html>";
+  String html ="<html> <head> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> <title>LED</title> </head> <body> <h1>LEDje</h1> <p><button onclick=\"aan()\">Aan</button></p> <p><button onclick=\"uit()\">Uit</button></p> <p><button onclick=\"toggle()\">Toggle</button></p> <script type=\"text/javascript\"> var xhttp = new XMLHttpRequest(); function aan() { xhttp.open(\"GET\", \"http://192.168.1.71/led?led=aan\", true); xhttp.send(); } function uit() { xhttp.open(\"GET\", \"http://192.168.1.71/led?led=uit\", true); xhttp.send(); } function toggle() { xhttp.open(\"GET\", \"http://192.168.1.71/led?led=toggle\", true); xhttp.send(); } </script> </body> </html>";
   server.send(200, "text/html", html);
 }
 
