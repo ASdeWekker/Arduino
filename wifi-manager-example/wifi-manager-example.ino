85228783789e34909b2dff2a186de86ece69438f
Alex de Wekker <alexdewekker6@gmail.com>
2019-03-24 15:51:04 +0100

    wifi manager working example

--- wifi-manager-example/wifi-manager-example.ino
+++ wifi-manager-example/wifi-manager-example.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
 
+#define led D4
+
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from mij");
+  digitalWrite(led, 0);
+}
+
+void setup() {
+  Serial.begin(230400);
+  pinMode(led, OUTPUT);
+
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("(_)_)::::::::D~~~", "wemos");
+
+  Serial.println(WiFi.localIP());
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("Server started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  server.handleClient();
 }
