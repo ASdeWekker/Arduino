31c5f680c1e80bce7887451091c5eb727a339eae
Alex de Wekker <alexdewekker6@gmail.com>
2019-03-28 21:44:53 +0100

    2 spaces to 4

--- sketches/ledstrip-desk/ledstrip-desk.ino
+++ sketches/ledstrip-desk/ledstrip-desk.ino
@@ -19,60 +19,60 @@ void stripColor(int r, int g, int b) {
 
 // Led request handling.
 void ledControl() {
-  if (server.arg("led") == "on") {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB(0, 255, 255);
-      FastLED.show();
-      delayMicroseconds(1);
+    if (server.arg("led") == "on") {
+        for (int i = 0; i < NUM_LEDS; i++) {
+            leds[i] = CRGB(0, 255, 255);
+            FastLED.show();
+            delayMicroseconds(1);
+        }
+        Serial.println("Led on");
+    } else if (server.arg("led") == "off") {
+        for (int i = 0; i < NUM_LEDS; i++) {
+            leds[i] = CRGB::Black;
+            FastLED.show();
+            delayMicroseconds(1);
+        }
+        Serial.println("Led off");
+    } else if (server.arg("led") == "toggle") {
+        if (check == HIGH) {
+            for (int i = 0; i < NUM_LEDS; i++) {
+                leds[i] = CRGB::Black;
+                FastLED.show();
+                delayMicroseconds(1);
+            }
+            Serial.println("Toggle off");
+            check = LOW;
+        } else {
+            for (int i = 0; i < NUM_LEDS; i++) {
+                leds[i] = CRGB(0, 255, 255);
+                FastLED.show();
+                delayMicroseconds(1);
+            }
+            Serial.println("Toggle on");
+            check = HIGH;
+        }
     }
-    Serial.println("Led on");
-  } else if (server.arg("led") == "off") {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB::Black;
-      FastLED.show();
-      delayMicroseconds(1);
-    }
-    Serial.println("Led off");
-  } else if (server.arg("led") == "toggle") {
-    if (check == HIGH) {
-      for (int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delayMicroseconds(1);
-      }
-      Serial.println("Toggle off");
-      check = LOW;
-    } else {
-      for (int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CRGB(0, 255, 255);
-        FastLED.show();
-        delayMicroseconds(1);
-      }
-      Serial.println("Toggle on");
-      check = HIGH;
-    }
-  }
-  server.send(200, "text/plain", "Processed \n");
+    server.send(200, "text/plain", "Processed \n");
 }
 
 void setup() {
-  // Setup basic stuff.
-  Serial.begin(230400);
+    // Setup basic stuff.
+    Serial.begin(230400);
 
-  // Wifi manager setup.
-  WiFiManager wifiManager;
-  wifiManager.autoConnect("(_)_)::::::::D~~~", "password");
-  Serial.println(WiFi.localIP());
+    // Wifi manager setup.
+    WiFiManager wifiManager;
+    wifiManager.autoConnect("(_)_)::::::::D~~~", "password");
+    Serial.println(WiFi.localIP());
 
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  FastLED.setBrightness(100);
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+    FastLED.setBrightness(100);
 
-  server.on("/led", ledControl);
-  server.begin();
-  Serial.println("Server started");
+    server.on("/led", ledControl);
+    server.begin();
+    Serial.println("Server started");
 }
 
 void loop() {
-  // Call the server
-  server.handleClient();
+    // Call the server
+    server.handleClient();
 }
