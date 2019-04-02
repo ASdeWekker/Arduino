e5dffde01e732e329e45a9bfe74670c423c4575c
Alex de Wekker <alexdewekker6@gmail.com>
2019-04-02 23:01:12 +0200

    removed unnecesary comments

--- sketches/fadeLed/fadeLed.ino
+++ sketches/fadeLed/fadeLed.ino
@@ -1,13 +1,11 @@
 #define led D2
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(led, OUTPUT);
   Serial.begin(230400);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   for (int i = 0; i < 256; i += 5) {
     analogWrite(led, i);
     delay(100);
