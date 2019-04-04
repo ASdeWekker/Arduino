e3175614c6848ea4046986c51574b25f9a5abe6f
Alex de Wekker <alexdewekker6@gmail.com>
2019-04-04 07:58:30 +0200

    syntax error

--- sketches/fadeLed/fadeLed.py
+++ sketches/fadeLed/fadeLed.py
@@ -6,13 +6,13 @@ led = machine.PWM(machine.Pin(2))
 def led_pulse(val):
 	if val % 20 == 0:
 		led.duty(val)
-		time_sleep(0.1)
+		time.sleep(0.1)
 
 while True:
 	for i in range(1024):
 		led_pulse(i)
-	time_sleep(0.5)
+	time.sleep(0.5)
 	for i in range(1023,-1,-1):
 		led_pulse(i)
-	time_sleep(0.5)
+	time.sleep(0.5)
 
