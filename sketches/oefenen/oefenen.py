105b17e9ffe096da19a5f0cb196db71921dbe84c
Alex de Wekker <alexdewekker6@gmail.com>
2019-04-06 20:26:30 +0200

    did some more practicing

--- sketches/oefenen/oefenen.py
+++ sketches/oefenen/oefenen.py
@@ -1,6 +1,14 @@
+'''
 from machine import Pin
 Pin(2, Pin.OUT).off()
 
-'''
 import pyb
 pyb.LED(2).off()'''
+
+import time
+
+count = 0
+while True:
+	print("ding", count)
+	count += 1
+	time.sleep(1)
