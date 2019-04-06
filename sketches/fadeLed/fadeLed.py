f9e0ad7a10d30bd30fad6f0f475da0be45245060
Alex de Wekker <alexdewekker6@gmail.com>
2019-04-06 21:40:28 +0200

    working again

--- sketches/fadeLed/fadeLed.py
+++ sketches/fadeLed/fadeLed.py
@@ -5,11 +5,11 @@ led = machine.PWM(machine.Pin(2))
 
 while True:
 	for i in range(0,1024,20):
-		led_pulse(i)
-                time.sleep(0.05
+		led.duty(i)
+		time.sleep_ms(50)
 	time.sleep(0.5)
 	for i in range(1023,-1,-20):
-		led_pulse(i)
-                time.sleep(0.05)
+		led.duty(i)
+		time.sleep_ms(50)
 	time.sleep(0.5)
-
+	
