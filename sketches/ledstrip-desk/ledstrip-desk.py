cade39b77df6fb3a906ff836cac02f6df0c98abc
Alex de Wekker <alexdewekker6@gmail.com>
2019-04-06 22:34:37 +0200

    working on connecting with a network and displaying something

--- sketches/ledstrip-desk/ledstrip-desk.py
+++ sketches/ledstrip-desk/ledstrip-desk.py
@@ -1,5 +1,24 @@
+
 import machine
 import time
-from dotenv import load_dotenv
+import network
+import socket
 
-load_dotenv()
+import wifi
+
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect(wifi.ssid,wifi.password)
+
+addr = socket.getaddrinfo("0.0.0.0", 80)[0][-1]
+
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+'''
+while True:
+	cl, addr = s.accept()
+	print("client connected from", addr)
+	cl.send("<h1>Alex de Wekker</h1>")
+	cl.close()
+'''
