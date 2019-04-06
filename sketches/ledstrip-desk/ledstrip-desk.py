6567a8a26e618f4b2eeeb36d5fc10b739877ab21
Alex de Wekker <alexdewekker6@gmail.com>
2019-04-06 23:37:11 +0200

    network not yet working

--- sketches/ledstrip-desk/ledstrip-desk.py
+++ sketches/ledstrip-desk/ledstrip-desk.py
@@ -3,19 +3,21 @@ import machine
 import time
 import network
 import socket
+
 # Importing the wifi credentials.
 import wifi
+
 # Enable the network and connecting to it.
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 sta_if.connect(wifi.ssid,wifi.password)
-
+'''
 addr = socket.getaddrinfo("0.0.0.0", 80)[0][-1]
 
 s = socket.socket()
 s.bind(addr)
 s.listen(1)
-'''
+
 while True:
 	cl, addr = s.accept()
 	print("client connected from", addr)
