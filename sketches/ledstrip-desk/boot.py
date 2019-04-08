f7dfadb1615b9dae913a0ba01d7131ee5344cb2e
Alex de Wekker <alexdewekker6@gmail.com>
2019-04-08 22:28:15 +0200

    it's finally working

--- sketches/ledstrip-desk/boot.py
+++ sketches/ledstrip-desk/boot.py
@@ -5,5 +5,22 @@ import gc
 esp.osdebug(None)
 gc.collect()
 
-import time
-import machine
+try:
+	import usocket as socket
+except:
+	import socket
+from machine import Pin
+import network
+import wifi
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(wifi.ssid,wifi.password)
+
+while wlan.isconnected() == False:
+	pass
+
+print("Connection succesful")
+print(wlan.ifconfig())
+
+led = Pin(2, Pin.OUT)
