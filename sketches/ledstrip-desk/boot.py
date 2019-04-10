87511b58440f0f1db402476e2c1aa56d2a176edd
Alex de Wekker <alexdewekker6@gmail.com>
2019-04-10 21:05:10 +0200

    removed try, except imports

--- sketches/ledstrip-desk/boot.py
+++ sketches/ledstrip-desk/boot.py
@@ -5,18 +5,8 @@ import gc
 esp.osdebug(None)
 gc.collect()
 
-try:
-	import usocket as socket
-except:
-	import socket
-try:
-	import utime as time
-except:
-	import time
-try:
-	import urequest as request
-except:
-	import request
+import socket
+import time
 from machine import Pin
 import network
 import wifi
