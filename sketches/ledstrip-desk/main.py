5cf2340ce990230c85decedb484204af7d9325c3
Alex de Wekker <alexdewekker6@gmail.com>
2019-04-10 21:05:25 +0200

    finished led_control and parser functions

--- sketches/ledstrip-desk/main.py
+++ sketches/ledstrip-desk/main.py
@@ -45,29 +45,53 @@ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind(("", 80))
 s.listen(5)
 
+check = False
+
+def led_control(arg):
+	if arg == "on":
+		print("led on")
+		led.on()
+		check = True
+	else if arg == "off":
+		print("led off")
+		led.off()
+		check = False
+	else if arg == "toggle":
+		if not check:
+			print("led on")
+			led.on()
+			check = True
+		else:
+			print("led off")
+			led.off()
+			check = False
+
+
+def led_fade():
+
+
+def pulse():
+
+
+def parser(get_request):
+	params = {}
+	arguments = get_request.split(" ")[1].split("&")
+	arguments[0] = arguments[0].replace("/","").replace("?","")
+
+	for args in arguments:
+		arg = args.split("=")
+		params[arg[0]] = arg[1]
+
+	if "state" in params:
+		led_control(params["state"])
+
+
 while True:
 	conn, addr = s.accept()
 	request_raw = conn.recv(1024)
 	request = str(request_raw)
-	request_enc = request_raw.decode("utf8")
-	led_on = request.find("/?led=on")
-	led_off = request.find("/?led=off")
 
-	if led_on == 6:
-		print("Led on")
-		print(led_on)
-		print(led_off)
-		led.value(1)
-		print("\nRaw\n", request_raw)
-		print("\nNormal\n", request)
-		print("\nEnc\n", request_enc)
-
-	if led_off == 6:
-		print("Led off")
-		print(led_on)
-		print(led_off)
-		led.value(0)
-		print(request)
+	parser(request)
 
 	conn.sendall("Processed.\n")
 	conn.close()
