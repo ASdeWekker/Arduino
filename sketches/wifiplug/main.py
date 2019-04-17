cafe243c8938353c08bebf02d5d505cc4f21ecd3
Alex de Wekker <alexdewekker6@gmail.com>
2019-04-17 22:36:10 +0200

    cleaned up the script and changed some names so it makes sense

--- sketches/wifiplug/main.py
+++ sketches/wifiplug/main.py
@@ -2,48 +2,38 @@ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind(("", 80))
 s.listen(5)
 
-def led_control(arg):
+
+def control(arg):
 	global check
 	if arg == "on":
-		print("led on")
-		led.off()
+		relay.off()
 		check = True
 	elif arg == "off":
-		print("led off")
-		led.on()
+		relay.on()
 		check = False
 	elif arg == "toggle":
 		if not check:
-			print("led on")
-			led.off()
+			relay.off()
 			check = True
 		else:
-			print("led off")
-			led.on()
+			relay.on()
 			check = False
 
 
 def parser(get_request):
 	params = {}
-	arguments = get_request.split(" ")[1].split("&")
-	arguments[0] = arguments[0].replace("/","").replace("?","")
-	print(arguments)
+	arguments = get_request.replace("/","").replace("?","").split(" ")[1].split("&")
 
 	for args in arguments:
 		arg = args.split("=")
 		params[arg[0]] = arg[1]
-	print(params)
 
-	if "state" in params:
-		led_control(params["state"])
+	if "power" in params:
+		control(params["power"])
 
 
 while True:
 	conn, addr = s.accept()
-	request_raw = conn.recv(1024)
-	request = str(request_raw)
-
-	parser(request)
-
+	parser(str(conn.recv(1024)))
 	conn.sendall("Processed.\n")
 	conn.close()
