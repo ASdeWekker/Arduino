d957877f1e8c533b31bc583b47778143abc86d98
Alex de Wekker <alexdewekker6@gmail.com>
2019-09-21 14:58:36 +0200

    added different speeds

--- sketches/other/fastled/src/arduino.ino
+++ sketches/other/fastled/src/arduino.ino
@@ -2,6 +2,11 @@
 
 CRGB leds[30];
 
+const int snel = 10;
+const int normaal = 50;
+const int langzaam = 100;
+const int nauwelijks = 200;
+
 void setup() {
 	FastLED.addLeds<NEOPIXEL, D4>(leds, 30);
 	Serial.begin(230400);
@@ -10,6 +15,6 @@ void setup() {
 void loop() {
 	static uint8_t hue = 0;
 	FastLED.showColor(CHSV(hue++, 255, 255));
-	delay(100);
+	delay(nauwelijks);
 	Serial.println(hue);
 }
