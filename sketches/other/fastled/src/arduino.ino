1ac4012a381ca9d2030602998156eb27f24c6d92
Alex de Wekker <alexdewekker6@gmail.com>
2019-09-25 19:56:38 +0200

    removed the serial stuff and made it slower

--- sketches/other/fastled/src/arduino.ino
+++ sketches/other/fastled/src/arduino.ino
@@ -6,15 +6,14 @@ const int snel = 10;
 const int normaal = 50;
 const int langzaam = 100;
 const int nauwelijks = 200;
+const int ultra_langzaam = 1000;
 
 void setup() {
 	FastLED.addLeds<NEOPIXEL, D4>(leds, 30);
-	Serial.begin(230400);
 }
 
 void loop() {
 	static uint8_t hue = 0;
 	FastLED.showColor(CHSV(hue++, 255, 255));
-	delay(normaal);
-	Serial.println(hue);
+	delay(ultra_langzaam);
 }
